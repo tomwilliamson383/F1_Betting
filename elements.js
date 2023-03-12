d700d6e6a453f776bcab62d8758ba6d900c3f1d7
Thomas Williamson <twil5596@uni.sydney.edu.au>
2023-03-12 13:44:19 +1100

    setup main

---
+++ elements.js
@@ -0,0 +1,8 @@
+// Update Tim's score to 10
+document.getElementById("tim-score").textContent = "10";
+
+// Update Tom's score to 5
+document.getElementById("tom-score").textContent = "5";
+
+// Update Charlie's score to 7
+document.getElementById("charlie-score").textContent = "7";
